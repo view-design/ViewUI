b93c57125ba57033073c760003738394b8e8aeb6
kuaifan <aipaw@live.cn>
2021-05-02 20:00:28 +0800

    Select选择器：在支持创建条目下输入框失去焦点时自动创建

--- src/components/select/select-head.vue
+++ src/components/select/select-head.vue
@@ -225,6 +225,10 @@
                 this.$emit('on-input-focus');
             },
             onInputBlur () {
+                if (this.allowCreate && this.query != '') {
+                    this.handleInputEnter();
+                    return;
+                }
                 if (this.showCreateItem) return;
                 if (!this.values.length) this.query = '';  // #5155
                 this.$emit('on-input-blur');
